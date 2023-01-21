8d34ecac5a03ae035e366bf64d205b8cb8a79462
bnbong <bbbong9@gmail.com>
2023-01-21 13:34:54 +0000

    #2.4 Maps

--- main.dart
+++ main.dart
@@ -1,11 +1,18 @@
 void main() {
-  var oldFriends = ['nico', 'lynn'];
-  var newFriends = [
-    'lewis',
-    'ralph',
-    'darren',
-    for(var friend in oldFriends) "^0^ $friend",
-  ];
+  var player = { // Map<String, Object> player
+    'name': 'nico',
+    'xp': 19.99,
+    'superpower': false,
+  };
 
-  print(newFriends);
+  Map<List<int>, bool> another_player = {
+    [1,2,3,4,5]: true,
+  };
+
+  List<Map<String, Object>> players = [
+    {
+      'name': 'nico',
+      'xp': 20000.99,
+    },
+  ]; // class is more better at API.
 }

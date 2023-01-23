5f44e2fc3c70d1791ad7bc3d88dd189a1ab9f41a
bnbong <bbbong9@gmail.com>
2023-01-23 00:43:29 +0000

    #4.2 better constructor

--- main.dart
+++ main.dart
@@ -1,18 +1,34 @@
 class Player {
   final String name;
   int xp;
+  String team;
+  int age;
 
-  Player(this.name, this.xp);
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
 
   void sayHello() {
     print("Hi my name is $name");
   }
 }
 
-
 void main() {
-  var player = Player("Junhyeok", 1500);
-  var player2 = Player("nico", 1000);
+  var player = Player(
+    name: "JunHyeok",
+    xp: 1200,
+    team: 'red',
+    age: 20,
+  );
+  var player2 = Player(
+    name: "nico",
+    xp: 1000,
+    team: 'blue',
+    age: 23,
+  );
 
   player.sayHello();
   player2.sayHello();
